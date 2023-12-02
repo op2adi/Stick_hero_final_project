e91e188cb368589d0c49a5c1e05b8140e86f1f15
op2adi <adityaupadhyay2708@gmail.com>
2023-12-02 23:42:25 +0530

    cherrydone

--- src/main/java/com/example/stick_hero_final_project/cheery.java
+++ src/main/java/com/example/stick_hero_final_project/cheery.java
@@ -6,6 +6,16 @@ import javafx.scene.image.ImageView;
 import java.util.Random;
 
 public class cheery {
+    public Double min(Double a,Double b){
+        if (a<b){
+            return a;
+        }
+        else {
+            return b;
+        }
+
+
+    }
     private ImageView cherry_image ;
 
     public ImageView getCherry_image() {
@@ -14,18 +24,18 @@ public class cheery {
     public cheery(Pillar p1,Pillar p2,Player_create player){
         double minX = 0;
         if(p1!=null){
-            minX = p1.getPillar().getX();
+            minX = p1.getPillar().getX()+p1.getPillar().getWidth();
         }
         else {
             minX = 60;
         }
-        double maxX = p2.getPillar().getX()-p2.getWidth();
+        double maxX = p2.getPillar().getX();
         Image cherry = new Image("D:\\Stick_Hero_Final_Project\\src\\main\\java\\com\\example\\stick_hero_final_project\\Images\\cherry.png") ;
         cherry_image = new ImageView(cherry);
         cherry_image.setFitWidth(30);
         cherry_image.setFitHeight(30);
         Random random = new Random();
-        double randomX = (maxX+minX)/2;
+        double randomX = minX -50;
         cherry_image.setX(randomX);
         boolean dirn = random.nextBoolean();
         if (dirn){
